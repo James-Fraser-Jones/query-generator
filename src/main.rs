ab754d430cb11cdf6997e06eed94fc64d501f653
smudgecat123 <jf14218@my.bristol.ac.uk>
2021-03-20 12:56:35 +0000

    sorted out input error handling

--- src/main.rs
+++ src/main.rs
@@ -6,31 +6,22 @@ use rand::prelude::*;
 use std::env;
 use std::fs::File;
 use std::io::prelude::*;
-use std::io::LineWriter;
+use std::io;
 
 /* examples (don't forget to use release build)
 benchmark_5m.txt 5000000 1000 100 1000
 benchmark_5k.txt 5000 100 50 100
 */
 
-fn main() -> std::io::Result<()> {
+fn main() -> io::Result<()> {
     //read args
-    let mut args = env::args();
-    args.next();
-    let file_path = args.next()
-    .expect("Error: expected \"file_path\" argument");
-    let query_num: usize = args.next()
-    .expect("Error: expected \"query_num\" argument").parse()
-    .expect("Error: failed to parse \"query_num\" argument");
-    let word_num = args.next()
-    .expect("Error: expected \"word_num\" argument").parse()
-    .expect("Error: failed to parse \"word_num\" argument");
-    let tag_num = args.next()
-    .expect("Error: expected \"tag_num\" argument").parse()
-    .expect("Error: failed to parse \"tag_num\" argument");
-    let add_query_history_num = args.next()
-    .expect("Error: expected \"add_query_history_num\" argument").parse()
-    .expect("Error: failed to parse \"add_query_history_num\" argument");
+    let args: Vec<String> = env::args().collect();
+    let file_path = args[1].to_owned();
+    let wrap_err = |e| io::Error::new(io::ErrorKind::InvalidInput, e);
+    let query_num: usize = args[2].parse().map_err(wrap_err)?;
+    let word_num: usize = args[3].parse().map_err(wrap_err)?;
+    let tag_num: usize = args[4].parse().map_err(wrap_err)?;
+    let add_query_history_num: usize = args[5].parse().map_err(wrap_err)?;
     
     //create generator
     let mut rng = thread_rng();
@@ -39,19 +30,19 @@ fn main() -> std::io::Result<()> {
 
     //open file and output buffer
     let file = File::create(file_path)?;
-    let mut writer = LineWriter::new(file);
+    let mut writer = io::BufWriter::new(file);
 
     //write number of queries as first line
-    writer.write(query_num.to_string().as_bytes())?;
-    writer.write(&[b'\n'])?;
+    writeln!(writer, "{}", query_num)?;
 
+    println!("Ready to generate queries");
     //write queries out to specified file
     for _ in 0..query_num {
         let query = generator.get_query(rng); //only thing which will grow is array of "active" indices
-        writer.write(&query.to_string().as_bytes())?;
-        writer.write(&[b'\n'])?;
+        writeln!(writer, "{}", query)?;
     }
     writer.flush()?;
+    println!("Done - generated {} queries", query_num);
 
     Ok(())
 }
